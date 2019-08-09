c4658028e545c26b25593b1ef593fe3ee4845f93
Dariusz Kulig <darek@MacBook-Air-Dariusz.local>
2019-08-09 19:05:42 +0200

    fixed formatting

--- src/main/java/sabre_interview/WordClassifier.java
+++ src/main/java/sabre_interview/WordClassifier.java
@@ -9,25 +9,26 @@ import java.util.TreeSet;
 
 /**
  * Class with solution for Sabre interview.
+ * 
  * @author Dariusz Kulig
  *
  */
 public class WordClassifier {
 
 	public static void main(String[] args) {
-		String testPhrase = "Authentication manager needed for password grant type.";
+		String testPhrase = "ala ma kota, kot koduje w Javie kota";
 		Map<Character, Set<String>> result = filterWords(testPhrase);
 		System.out.println(result);
 	}
 
-	
 	/**
-     * Static method classifying letters and words containing them.
-     * Letters and words are also sorted alphabetically. If input is null or empty,
-     * it will return empty collection. 
-     * @param testPhrase - sentence to be filtered.
-     * @return Map<Character, Set<String>> containing result of filtration.
-     */
+	 * Static method classifying letters and words containing them. Letters and
+	 * words are also sorted alphabetically. If input is null or empty, it will
+	 * return empty collection.
+	 * 
+	 * @param testPhrase - sentence to be filtered.
+	 * @return Map<Character, Set<String>> containing result of filtration.
+	 */
 	public static Map<Character, Set<String>> filterWords(String testPhrase) {
 		Map<Character, Set<String>> words = new LinkedHashMap<>();
 		if (testPhrase == null || testPhrase.trim().isEmpty()) {
@@ -36,7 +37,7 @@ public class WordClassifier {
 		String filteredPhrase = testPhrase.trim().toLowerCase().replaceAll("[^\\sa-z]", "");
 		List<String> tokens = Arrays.asList(filteredPhrase.split("\\s"));
 		int[] chars = filteredPhrase.chars().filter(Character::isLetter).distinct().sorted().toArray();
-		
+
 		Arrays.stream(chars).forEach(ch -> {
 			tokens.forEach(token -> {
 				if (token.chars().anyMatch(c -> ch == c)) {
